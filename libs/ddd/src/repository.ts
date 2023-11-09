f468f6dbcd1cf1defe940743657dc23e555e702d
Adolfo Yasser Santana Rojas <aysajor23@gmail.com>
2023-11-09 17:17:53 -0500

    Refactor return promise delete'method

--- libs/ddd/src/repository.ts
+++ libs/ddd/src/repository.ts
@@ -4,6 +4,6 @@ import { Entity, EntityProps } from "./entity";
 export interface MutableRepository< E extends Entity<EntityProps>> {
     add( entity: E): Promise< Result<E>>
     update( entity: E): Promise< Result<true>>
-    delete(identifier: any)
+    delete(identifier: any):Promise< Result<true>>
 }
 
