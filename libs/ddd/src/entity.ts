26f4ce2134dbb6299d103b2049c7c9a2689192d7
Adolfo Yasser Santana Rojas <aysajor23@gmail.com>
2023-11-09 19:02:43 -0500

    Add id property in entity class

--- libs/ddd/src/entity.ts
+++ libs/ddd/src/entity.ts
@@ -3,8 +3,17 @@ export interface EntityProps {
     [key: string]: any
 }
 
-export class Entity< E extends EntityProps>{
+export class Entity< E extends EntityProps, I = string>{
+    private _id: I
     constructor( protected _props: E) {}
+
+    get id(): I {
+        return this._id
+    }
+
+    set id( value: I) {
+        this._id = value
+    }
 
     get props(): EntityProps {
         return Object.freeze( this._props)
