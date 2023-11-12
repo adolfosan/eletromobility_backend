b11e2c1b7bc1305fb0ab51cf6ddec013fe54e0c9
Adolfo Yasser Santana Rojas <aysajor23@gmail.com>
2023-11-12 09:59:26 -0500

    Add plugusecase contract

---
+++ apps/locations-service/src/plugs/application/create.plug/contract.ts
@@ -0,0 +1,5 @@
+import { PromiseResult, UseCase } from "@libs/logic";
+import { PlugProps } from "@locations-service/plugs/domain";
+
+export type CreatePlugDTO = PlugProps
+export interface ICreatePlugUseCase extends UseCase<CreatePlugDTO, PromiseResult<true>> {}
