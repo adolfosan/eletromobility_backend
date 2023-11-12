d7c63366680447b5b6c68c5489483b296143fb3b
Adolfo Yasser Santana Rojas <aysajor23@gmail.com>
2023-11-12 09:59:52 -0500

    Revert "Add plugusecase contract"
    This reverts commit b11e2c1b7bc1305fb0ab51cf6ddec013fe54e0c9.

--- apps/locations-service/src/plugs/application/create.plug/contract.ts
+++
@@ -1,5 +0,0 @@
-import { PromiseResult, UseCase } from "@libs/logic";
-import { PlugProps } from "@locations-service/plugs/domain";
-
-export type CreatePlugDTO = PlugProps
-export interface ICreatePlugUseCase extends UseCase<CreatePlugDTO, PromiseResult<true>> {}
